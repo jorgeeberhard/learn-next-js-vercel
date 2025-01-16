6c5916a1b7b51a4f5fdd173f2d076511daedf1b4
Jorge Eberhard <jorgem.eberhardc@gmail.com>
2025-01-16 16:45:01 -0300

    add `chapter 16` contents

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,23 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import CustomersTable from "@/app/ui/customers/table";
+import Search from "@/app/ui/search";
+import { lusitana } from "@/app/ui/fonts";
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+
+  return (
+    <div className="w-full">
+      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+        Customers
+      </h1>
+      <Search placeholder="Search customers..." />
+
+      <CustomersTable query={query}></CustomersTable>
+    </div>
+  );
 }
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,7 +4,8 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-import { error } from "console";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 
 const FormSchema = z.object({
   id: z.string(),
@@ -55,6 +56,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error) {
+    console.error(error);
     return {
       message: "Database Error: Failed to Create Invoice.",
     };
@@ -93,6 +95,7 @@ export async function updateInvoice(
       WHERE id = ${id}
     `;
   } catch (error) {
+    console.error(error);
     return {
       message: "Database Error: Failed to Udpate Invoice",
     };
@@ -108,8 +111,25 @@ export async function deleteInvoice(id: string) {
     `;
     revalidatePath("/dashboard/invoices");
   } catch (error) {
-    return {
-      message: "Database Error: Failed to Delete Invoice",
-    };
+    console.error(error);
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong";
+      }
+    }
+    throw error;
   }
 }
